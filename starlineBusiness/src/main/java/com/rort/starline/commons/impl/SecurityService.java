2981f10379a7e2fe1471cab138215c6db3dcb7c0
Reinel Ortiz <rortiz@conexia.com>
2017-08-04 17:13:39 -0500

    Configuración Contexto de Persistencia

--- starlineBusiness/src/main/java/com/rort/starline/commons/impl/SecurityService.java
+++ starlineBusiness/src/main/java/com/rort/starline/commons/impl/SecurityService.java
@@ -3,9 +3,14 @@ package com.rort.starline.commons.impl;
 import com.rort.starline.commons.def.ISecurityService;
 import com.rort.starline.commons.dto.UserDto;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
 public class SecurityService implements ISecurityService {
+
+	@PersistenceContext(unitName = "starlinePU")
+	private EntityManager em;
 
 	@Override
 	public UserDto userAuthentication(final String userName, final String password) {
